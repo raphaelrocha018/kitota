7ab634d33c0dcf89f5446c115ffff5cc790e2d4f
Raphael Rocha <raphaelrocha18@outlook.com>
2023-06-09 09:19:18 -0300

    update

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -1,7 +1,7 @@
 export default function initAnimaScroll() {
     const sections = document.querySelectorAll('[data-anime="scroll"]');
     if (sections.length) {
-        const windowMetade = window.innerHeight * 0.6;
+        const windowMetade = window.innerHeight * 0.5;
 
         function animaScroll() {
             sections.forEach((section) => {
