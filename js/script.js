c1333c9ee4ba85dfd1610248c43f1433c584ff0c
Raphael Rocha <raphaelrocha18@outlook.com>
2023-06-09 00:14:23 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// const item1 = document.querySelector('[data-inicio="a"]');
-
-// const movimento1 = new KeyframeEffect(item1, [{ opacity: 0 }, { opacity: 1 }], {
-//     delay: 1000,
-//     duration: 2000,
-//     fill: "forwards",
-// });
-
-// const item1Animation = new Animation(movimento1, document.timeline);
-// item1Animation.play();
-
 import initAnimaScroll from "./modules/anima-scroll.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 
